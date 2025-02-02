94e16b0d846d7941fa2f9f72c1e62134e4125c21
Sumanth <tonysumanth19@gmail.com>
2025-02-02 15:26:54 +0530

    Python Practice Work

---
+++ functions/program.py
@@ -0,0 +1,53 @@
+def calcTime(args):
+    def inner():
+        import time
+        t1=time.time()
+        args()
+        t2=time.time()
+        print(f'Time = {t2-t1}')
+    return inner
+@calcTime
+def fibo():
+    fn=int(input('Enter first numer:'))
+    sn=int(input('Enter second numer:'))
+    n=int(input('No.of values:'))
+    if n==1:
+        print(fn,end=' ')
+    elif n==2:
+        print(fn,sn,end=' ')
+    else:
+        print(fn,sn,end=' ')
+        for i in range(n-2):
+            tn=fn+sn
+            print(tn,end=' ')
+            fn,sn=sn,tn
+        print()
+@calcTime
+def primeNumbers():
+    LL=int(input('Enter the lower range:'))
+    UL=int(input('Enter the Upper range:'))
+    count=0
+    L=[]
+    for n in range(LL,UL+1):
+        if n>0:
+            for i in range(2,n//2+1):
+                if n%i==0:
+                    break
+            else:
+                L.append(n)
+    print(L)
+
+@calcTime
+def palindromeNum():
+    n=int(input('Enter a number:'))
+    rev=0
+    dummy=n
+    while dummy>0:
+        r=dummy%10
+        dummy//=10
+        rev=rev*10+r
+    if n==rev:
+        print(True)
+    else:
+        print(False)
+palindromeNum()
